21428d16efe018d94c2d253b8419ffa8a4c694c9
KaterynaLovushkina <Kateryna.Lovushkina.IR.2021@lpnu.ua>
2022-06-09 11:34:07 +0300

    add modified lab 3 code

--- LoggerManager.py
+++ LoggerManager.py
@@ -1,15 +1,26 @@
 import re
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s: %(name)s:  %(message)s)")
+
+file_handler = logging.FileHandler('regex.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 
 class LoggerManager:
     @staticmethod
-    def print_data_contains_regex(filename: str):
+    def print_count_of_data_contains_regex(filename: str):
         regex = r'(21).+.+(php).+(HTTP).+'
         count = 0
         with open(filename) as f:
             lines = f.readlines()
             for line in lines:
                 find_regex = re.search(regex, line)
-                if find_regex is not None:
-                    print(find_regex.group())
+                if find_regex :
+                    logger.info(find_regex.group())
                     count += 1
         print(f"Count of matches: {count}")
